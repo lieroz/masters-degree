c471456a8fc65c443643d198dd1dc4ce57ab114b
Andrey Kamakin <a.kamakin@icloud.com>
2021-04-30 17:28:30 +0300

    allocator initial
    delete

---
+++ allocator/allocator.h
@@ -0,0 +1,120 @@
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <cassert>
+#include <cstring>
+#include <string>
+#include <string_view>
+#include <atomic>
+
+class SpinLock
+{
+public:
+    void lock() noexcept
+    {
+        for (;;)
+        {
+            if (!lock_.exchange(true, std::memory_order_acquire))
+            {
+                return;
+            }
+            while (lock_.load(std::memory_order_relaxed))
+            {
+                __builtin_ia32_pause();
+            }
+        }
+    }
+
+    bool tryLock() noexcept
+    {
+        return !lock_.load(std::memory_order_relaxed) &&
+               !lock_.exchange(true, std::memory_order_acquire);
+    }
+
+    void unlock() noexcept
+    {
+        lock_.store(false, std::memory_order_release);
+    }
+
+private:
+    std::atomic<bool> lock_{false};
+};
+
+template<typename Lock>
+class LockGuard
+{
+public:
+    LockGuard(Lock &lock) : lock_(lock)
+    {
+        lock_.lock();
+    }
+
+    ~LockGuard()
+    {
+        lock_.unlock();
+    }
+
+private:
+    Lock &lock_;
+};
+
+std::string systemError(std::string_view message)
+{
+    return std::string(message) + " failed : " + std::strerror(errno);
+}
+
+uint64_t roundUpToNextPowerOf2(uint64_t value)
+{
+    value--;
+    value |= value >> 1;
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    value |= value >> 32;
+    value++;
+    return value;
+}
+
+template<typename T>
+class MemoryQueue
+{
+public:
+    MemoryQueue() noexcept : pageSize(getpagesize())
+    {
+        sizeClass = roundUpToNextPowerOf2(sizeof(T));
+
+        tail = static_cast<Header *>(
+            mmap(nullptr, pageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+        assert(head != MAP_FAILED && systemError("mmap").c_str());
+
+        head = tail + pageSize / sizeof(Header) - 1;
+    }
+
+    bool push(const T &value) noexcept
+    {
+        LockGuard guard{lock};
+        return true;
+    }
+
+    bool pop(T &value) noexcept
+    {
+        LockGuard guard{lock};
+        return true;
+    }
+
+private:
+    struct Header
+    {
+        Header *next{nullptr};
+    };
+
+private:
+    uint64_t pageSize;
+    uint64_t sizeClass;
+
+    Header *head{nullptr};
+    Header *tail{nullptr};
+
+    SpinLock lock;
+};
